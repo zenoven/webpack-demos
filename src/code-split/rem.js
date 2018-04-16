f896c235f9aed871a8ec6601e5b60866dd165521
zenoven <zenoven@gmail.com>
2018-04-16 22:15:41 +0800

    修正字体大小计算错误

--- src/code-split/rem.js
+++ src/code-split/rem.js
@@ -1,12 +1,14 @@
 (function(){
-  var basis = 20
-  var MAX_WIDTH = 750
+  var basis = 100 // px转为rem时计算基准值
+  var MAX_WIDTH = 750 // 超出此宽度后页面居中显示，且字体不再增大
+  var UI_WIDTH = 750 //UI稿宽度
+  var SCALE = 2 //UI稿放大倍数
   var rootElement = document.documentElement
   var resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize'
   var calcFontSize = function(){
     var screenWidth = rootElement.clientWidth
     var width = screenWidth < MAX_WIDTH ? screenWidth : MAX_WIDTH
-    var fontSize = width / basis
+    var fontSize = basis * (width / UI_WIDTH) / SCALE
     rootElement.style.fontSize = fontSize + 'px'
     if(screenWidth > MAX_WIDTH) {
       rootElement.style.margin = 'auto'
