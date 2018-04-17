218dc2dc4925e981a7bd441e8df14d19372f7345
zenoven <zenoven@gmail.com>
2018-04-17 16:07:29 +0800

    update compiler tips

--- scripts/server.js
+++ scripts/server.js
@@ -15,7 +15,39 @@ const compiler = webpack(config)
 const appConfig = yaml.safeLoad(fs.readFileSync(`${projectDir}/config/app.yml`))
 const {server: {devPort}} = appConfig
 
+compiler.plugin('compile', (stats) => {
+  console.log('webpack compiling...')
+})
+
+compiler.plugin('done', (stats) => {
+  const time = (stats.startTime - stats.endTime) / 1000
+
+  if(stats.hasErrors()){
+    console.log('webpack build error')
+
+    return console.log(stats.toString({
+      colors: true,
+      timings: false,
+      hash: false,
+      version: false,
+      assets: false,
+      reasons: false,
+      chunks: false,
+      children: false,
+      chunkModules: false,
+      modules: false
+    }))
+  }
+  console.log('webpack build successfully')
+  console.log('stats', stats)
+})
+
 const server = new WebpackDevServer(compiler, {
+  compress: true,
+  disableHostCheck: true,
+  watchOptions: {
+    aggregateTimeout: 300
+  },
   stats: {
     colors: true
   }
