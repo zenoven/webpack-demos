d963e200737eeb793e2cd880e1c88d798709487d
zenoven <zenoven@gmail.com>
2018-04-19 10:54:37 +0800

    update long-list

--- src/code-split/long-list.js
+++ src/code-split/long-list.js
@@ -1,6 +1,7 @@
 let count = [10000, 50000, 100000]
 let times = 10
 let logs = []
+window.logs = logs
 module.exports = {
   // calcTime(func, timeLogs){
   //   var start = new Date()
@@ -60,42 +61,57 @@ module.exports = {
       subtree: true,
       childList: true
     }
-    let j = 0;
-    let result = run(10000)
-
+    let test = run(10000)
+    // while(!result.next().done){
+    //   result.next()
+    // }
     window.getResult = () => {
-      if(j < times) {
-        result.next()
+      let res = test.next()
+      if(!res.done) {
+        test.next()
         console.log('logs:', logs)
-        return j++
       }
-      return console.log('done')
+      console.log('done with logs:', logs)
     }
 
     function* run(count){
+      debugger
       let i = 0
       while(i < times) {
-
+        console.log('--------i:', i)
+        let currentQueue = functions.slice()
+        console.log('--------currentQueue:', currentQueue)
+        let currentTask
         logs[i] = {}
-        yield Promise.all(functions.map(func => {
-          logs[i][func.name] = {startTime: Date.now()}
-          return new Promise( (resolve, reject) => {
+        while(currentTask = currentQueue.shift()) {
+          console.log('--------currentTask:', currentTask)
+          logs[i][currentTask.name] = {startTime: Date.now()}
+          debugger
+          yield new Promise( (resolve, reject) => {
             let observer = new MutationObserver( (mutations, observer) => {
+              console.log('--------in MutationObserver callback, i:',)
               observer.disconnect()
               self.emptyContent()
-              logs[i][func.name].endTime = Date.now()
-              logs[i][func.name].duration = logs[i][func.name].endTime - logs[i][func.name].startTime
+              logs[i][currentTask.name].endTime = Date.now()
+              logs[i][currentTask.name].duration = logs[i][currentTask.name].endTime - logs[i][currentTask.name].startTime
               resolve()
             })
             observer.observe(target, options)
-            func(count)
+            currentTask(count)
           })
-        }))
+        }
         i++
       }
+      console.log('done')
     }
 
 
+  },
+
+  sleep(duration){
+    return new Promise( (resolve, reject) => {
+      setTimeout(resolve, duration)
+    } )
   },
 
   start(){
