ece6ff410d201002e095cdc5143c601a15db1b8a
zenoven <zenoven@gmail.com>
2018-04-26 08:15:19 +0800

    add 数组去重方法

--- src/code-split/unique.js
+++ src/code-split/unique.js
@@ -3,7 +3,31 @@ function uniqueWithSet(arr) {
 }
 
 function uniqueWithIndexOf(arr){
-  
+  let result = []
+  for(let i = 0; i < arr.length; i++){
+    if(result.indexOf(arr[i]) === -1){
+      result.push(arr[i])
+    }
+  }
+  return result
+}
+
+function uniqueWithDoubleLoop(arr) {
+  let result = []
+  for(let i = 0, arrLen = arr.length; i < arrLen; i++){
+
+    for(var j = 0, resultLen = result.length; j < resultLen; j++) {
+      if(arr[i] === result[j]) {
+        break
+      }
+    }
+
+    // 内层循环执行完j === resultLen，说明当前result数组中不存在arr[i]，即arr[i]是唯一的
+    if(j === resultLen){
+      result.push(arr[i])
+    }
+  }
+  return result
 }
 
 function uniqueWithObject(arr){
