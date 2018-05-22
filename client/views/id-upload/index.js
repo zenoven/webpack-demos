44bddf9e3516edf775422f37986893982fb338ed
zenoven <zenoven@gmail.com>
2018-05-22 11:27:08 +0800

    update image preview

--- client/views/id-upload/index.js
+++ client/views/id-upload/index.js
@@ -19,7 +19,7 @@ let page = {
         this.data[i] = {
           input: document.getElementById(id),
           reader: new FileReader(),
-          image: new Image(),
+          image: document.getElementById(id).nextElementSibling.getElementsByClassName('thumbnail')[0],
           canvas: document.createElement('canvas'),
         }
 
@@ -27,27 +27,30 @@ let page = {
 
         this.data[i].input.addEventListener('change', (e) => {
           let file = e.target.files[0]
+          if(!file) return
           if(file.type.indexOf('image') === 0) {
             this.data[i].reader.readAsDataURL(file)
           }
         })
 
-        this.data[i].reader.onload = (e) => {
+        this.data[i].reader.addEventListener('load', (e) => {
           this.data[i].image.src = e.target.result
+          console.log(this.data[i].image)
           console.log('result:\n')
           console.log(e.target.result)
-        }
+        })
 
         // 图片压缩处理，不超过最大尺寸
         // 非箭头函数
-        this.data[i].image.onload = function () {
+        this.data[i].image.addEventListener('load', function () {
           let originWidth = this.width
           let originHeight = this.height
 
+          console.log('process')
           if(originWidth > MAX_WIDTH || originHeight > MAX_HEIGHT) {
 
           }
-        }
+        }, false)
       })
 
     }, false)
